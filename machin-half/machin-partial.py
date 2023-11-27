418cd809190e4e34b7ee87d9a08fa2bc054c2a36
Secoh <oalferov1@gmail.com>
2023-11-26 22:17:59 -0800

    Machin-half article, long int

---
+++ machin-half/machin-partial.py
@@ -0,0 +1,90 @@
+import math;
+
+def log10(x) :
+    return math.log(x) / math.log(10);
+
+def str_x_or_lg(nm, x) :   # def. lg(x) := log10(x)
+    if x < 1e16 :
+        return nm + " " + str(x);
+    return "lg " + nm + " " + str(log10(x));
+
+part = int(input("Partial ? (1=yes, 0=no) > "));
+Q = int(input("Start Q > "));
+
+A = 1;
+B = 1;
+QS = 1;
+M = 0;
+Q0 = Q;
+QQ = [];
+br = 0;
+
+# first term
+while A*Q >= B :
+    print(str_x_or_lg("A", A), str_x_or_lg("B", B));
+
+    M = M + 1;
+    tmpA = A * Q - B;
+    B = B * Q + A;
+    A = tmpA;
+
+# check other approximation for the first term
+tmpA = A * Q - B;
+tmpB = B * Q + A;
+if A * tmpB + B * tmpA > 0 :
+    QS = -1;
+    M = M + 1;
+    A = -tmpA;
+    B = tmpB;
+    print(str_x_or_lg("A", A), str_x_or_lg("B", B));
+
+print("M", M, "\n---");
+
+# higher terms
+while A > 0 :
+    print(str_x_or_lg("A", A), str_x_or_lg("B", B));
+
+    Q = (B + A - 1) // A;
+
+    tmpA = A * Q - B;
+    tmpA1 = tmpA - A;
+
+    if abs(tmpA) > abs(tmpA1) :
+        Q = Q - 1;
+        tmpA = tmpA1;
+
+    B = B * Q + A;
+    A = tmpA;
+
+    QQ.append(QS * Q);  # trace the sign in q_k
+
+    if A < 0 :
+        A = -A;
+        QS = -QS;
+
+    if part > 0 and log10(Q) > 1000000 :
+        print("break");
+        br = 1;
+        break;  # for partial series
+
+# Lehmer measure
+L = 1 / log10(Q0);
+print("---\nM", M, str_x_or_lg("Q", Q0));
+for q1 in QQ :
+    L = L + 1 / log10(abs(q1));
+    str_sign = ("(+)" if q1>0 else "(-)");
+    print(str_sign, str_x_or_lg("Q", abs(q1)));
+
+if br > 0 :  # remaining sum in the partial series is less 
+    L = L + 1 / log10(abs(QQ[-1]));     # than the last term
+
+# Pi sum for sanity check
+S = M*math.atan(1/Q0);
+for q1 in QQ :
+    S = S + math.atan(1/q1);
+
+say = ("(brk)\n---\nLem <" if br>0 else "---\nLem");
+print(say, L, "\nPi", 4*S);
+
+# notable fraction: 22/7
+# https://www.britannica.com/science/pi-mathematics
---
+++ test-int-newton-div/test-int-newton-div.cpp
@@ -0,0 +1,116 @@
+// test-int-newton-div.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#define SKLIB_TARGET_TEST
+#include "SKLib/include/types.hpp"
+#include "SKLib/include/utility.hpp"
+#include "SKLib/include/bitwise.hpp"
+#include "SKLib/include/timer.hpp"
+
+// model with normal integers
+bool mdiv(uint32_t A, uint32_t B, uint32_t& Q, uint32_t& R, uint64_t salt)
+{
+    constexpr unsigned base_bits_max = sklib::bits_width_v<uint32_t>;
+    const unsigned base_bits_min = sklib::bits_rank(A);
+    const unsigned base_bits = base_bits_min + salt % (base_bits_max-base_bits_min+1);
+
+    const uint64_t base = (uint64_t)1 << base_bits;
+    const unsigned B_rank = sklib::bits_rank(B);
+
+    // 0. special cases
+
+    if (!B) return (R=1), false;
+
+    if (B_rank >= base_bits)
+    {
+        if (B_rank != base_bits || A < B)
+        {
+            Q = 0;
+            R = A;
+        }
+        else
+        {
+            Q = 1;
+            R = A - B;
+        }
+
+        return true;
+    }
+
+/*
+    if (B_rank > base_bits)
+    {
+        Q = 0;
+        R = A;
+        return true;
+    }
+
+    if (B_rank == base_bits)
+    {
+        if (A < B)
+        {
+            Q = 0;
+            R = A;
+        }
+        else
+        {
+            Q = 1;
+            R = A - B;
+        }
+
+        return true;
+    }
+*/
+
+    // 1. divide base by B
+
+    uint64_t x = (uint64_t)1 << (base_bits - B_rank);
+
+    while (true)
+    {
+        auto x1 = ((x << (base_bits+1)) - B*x*x) >> base_bits;
+        if (x1 < x) return (R=2), false;
+        if (x1 == x) break;
+        x = x1;
+    }
+
+    // 1.a. quotent correction
+
+    if (base > B * (x+1)) x++;
+
+    // 2. multiply by dividend, correct quotent again
+
+    Q = (uint32_t)((x*A) >> base_bits);
+
+    R = A - Q*B;
+    if (R >= B) { Q++; R-=B; }
+
+    return true;
+}
+
+int main()
+{
+    sklib::random_size_integer_device<uint32_t, 32> RND;
+    sklib::timer_stopwatch_type strobe(1000);
+
+    uint64_t K = 0;
+    while (true)
+    {
+        auto A = RND();
+        auto B = RND();
+        if (!B) continue;
+        uint32_t Q=0, R=0;
+        if (!mdiv(A, B, Q, R, K) || R >= B || A != B*Q + R)
+        {
+            std::cout << "A=" << A << " B=" << B << " Q=" << Q << " R=" << R << " (K=" << K << ")\n";
+            return -1;
+        }
+
+        if (K++, strobe(true)) std::cout << K << "\r" << std::flush;
+    }
+
+    // return 0;
+}
+
